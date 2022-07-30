9bf879452500ee8a25e6c54ec777b484a247f328
Luc-ceba <ceballos.luciano3110@gmail.com>
2022-07-30 17:30:23 -0300

    fin cap. 2 POO

--- 2-POO/2-Crear-Objetos.rb
+++ 2-POO/2-Crear-Objetos.rb
@@ -79,3 +79,91 @@ module Pepita
   end
 end
 
+// 11
+module Obera
+  def self.kilometro
+    return 1040
+  end
+end
+
+module Iruya
+  def self.kilometro
+    return 1710
+  end  
+end
+
+module BuenosAires
+  def self.kilometro
+    return 0
+  end
+end
+
+module Pepita
+  @energia = 1000
+  @ciudad = Obera
+  
+  def self.energia
+    @energia 
+  end
+  
+  def self.ciudad
+    @ciudad
+  end
+  
+  def self.cantar!
+    'pri pri pri'
+  end
+  
+  def self.comer_lombriz!
+    @energia += 20
+  end
+  
+  def self.volar_en_circulos!
+    @energia -= 10
+  end
+  
+  def self.volar_hacia!(destino)
+    @energia -=  ((@ciudad.kilometro - destino.kilometro).abs) / 2
+    @ciudad = destino
+  end
+end
+
+
+// Delegar, metodos distancia a y gastar_energia
+module Pepita
+  @energia = 1000
+  @ciudad = Obera
+  
+  def self.energia
+    @energia 
+  end
+  
+  def self.ciudad
+    @ciudad
+  end
+  
+  def self.cantar!
+    'pri pri pri'
+  end
+  
+  def self.comer_lombriz!
+    @energia += 20
+  end
+  
+  def self.volar_en_circulos!
+    @energia -= 10
+  end
+  
+  def self.volar_hacia!(destino)
+    self.gastar_energia!(destino)
+    @ciudad = destino
+  end
+
+  def self.distancia_a(destino)
+   (@ciudad.kilometro - destino.kilometro).abs
+  end
+  
+  def self.gastar_energia!(destino)
+   @energia -= (distancia_a(destino).abs / 2)
+ end
+end
